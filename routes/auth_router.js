f911639650598b3a5962c11f03249861984fd1ae
Jake <panamajake86@gmail.com>
2020-04-27 19:24:08 -0600

    Eliminated unneccesary middleware. Placed the error handling in the endpoints themselves.

--- routes/auth_router.js
+++ routes/auth_router.js
@@ -1,28 +1,47 @@
 const router = require('express').Router();
 const bc = require('bcryptjs');
 
-const regMiddleware = require('../middleware/register-error-middleware');
-const loginMiddleware = require('../middleware/login-error-middleware');
-
-router.post('/register', regMiddleware, async (req, res) => {
+router.post('/register', async (req, res) => {
     const data = req.body;
     data.password = bc.hashSync(data.password, 12);
 
     try {
-        const reg = await Users.add(data);
-        res.status(201).json(reg, token);
+        const { username, email, password } = req.body;
+        if (!username) {
+            res.status(404).json({ message: 'Please provide your username!' });
+        } else {
+            if (!email) {
+                res.status(404).json({ message: 'Please provide your email!' });
+            } else {
+                if (!password) {
+                    res.status(404).json({ message: 'Please provide a password!' });
+                } else {
+                    const reg = await Users.add(data);
+                    res.status(201).json(reg, token);
+                }
+            }
+        }
     } catch (err) {
         res.status(500).json({ message: 'Could not register user, please try again later.', err });
     }
 });
 
-router.post('/login', loginMiddleware, async (req, res) => {
+router.post('/login', async (req, res) => {
     const { email, password } = req.body;
     try {
-        const log = await Users.findBy({email}).first();
+        const log = await Users.findBy({ email }).first();
+        const { username, password } = req.body;
         if (log && bc.compareSync(password, log.password)) {
-            const token = genToken(log);
-            res.status(200).json({ message: `Welcome ${log.username}!`, token: token });
+            if (!username) {
+                res.status(404).json({ message: 'Please provide your username!' });
+            } else {
+                if (!password) {
+                    res.status(404).json({ message: 'Please provide a password!' });
+                } else {
+                    const token = genToken(log);
+                    res.status(200).json({ message: `Welcome ${log.username}!`, token: token });
+                }
+            }
         } else {
             res.status(401).json({ message: 'Credentials incorrect, please try again.', err });
         }
