40ce77f3a139f547174eea227952b90403903589
Jake <panamajake86@gmail.com>
2020-04-28 21:48:56 -0600

    syntax

--- routes/auth_router.js
+++ routes/auth_router.js
@@ -34,7 +34,7 @@ router.post("/register", async (req, res) => {
 });
 
 router.post("/login", async (req, res) => {
-  const { email, password } = req.body;
+  const { username, password } = req.body;
   try {
     const log = await Users.findBy({ email }).first();
 
