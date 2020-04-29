5b8f0ef309840b31b782314b86821b3bf233f752
Jake <panamajake86@gmail.com>
2020-04-28 20:23:39 -0600

    built out the model to connect the routes to the db.

---
+++ models/users_model.js
@@ -0,0 +1,45 @@
+const db = require('../data/dbConfig');
+
+module.exports = {
+    add,
+    find,
+    findBy,
+    findById,
+    update,
+    remove
+};
+
+async function add(data) {
+    const [id] = await db('users').insert(data, "id");
+
+    return findById(id);
+};
+
+function find() {
+    return db('users')
+        .select('username', 'email', 'first_name', 'last_name');
+};
+
+function findBy(filter) {
+    return db('users')
+        .where(filter);
+};
+
+function findById(id) {
+    return db('users')
+        .where({ id })
+        .select('username', 'email', 'first_name', 'last_name')
+        .first();
+};
+
+function update(data, id) {
+    return db('users')
+        .where({ id })
+        .update(data);
+};
+
+function remove(id) {
+    return db('users')
+        .where(id)
+        .del();
+};
