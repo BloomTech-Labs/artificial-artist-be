54860759cafda621f4bbe8d92e8fd48d4c23d098
Jake <panamajake86@gmail.com>
2020-04-29 22:17:43 -0600

    changed the restricted protectiion to be for the route instead of per endpoint.

--- api/api_router.js
+++ api/api_router.js
@@ -1,13 +1,13 @@
 const router = require('express').Router();
-
+const restricted = require('../middleware/restricted_middleware');
 const authRouter = require('../routes/auth_router');
 const userRouter = require('../routes/user_router');
 const video_create = require('../routes/video_create');
 const video_list = require('../routes/video_list');
 
 router.use('/auth', authRouter);
-router.use('/user', userRouter);
-router.use('/create-video', video_create);
+router.use('/user', restricted, userRouter);
+router.use('/create-video', restricted, video_create);
 router.use('/video-list', video_list);
 
 router.get('/', (req, res) => {
