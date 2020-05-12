6d31a10995631fbf857249698ebaaa47597d2d13
Jake <panamajake86@gmail.com>
2020-05-11 21:33:33 -0600

    Refactored the video route so that we are protecting the endpoint for the POST and using the same route. I then deleted the create-video route. Created the update-video route for DS to update the url in the video location. It is unprotected but DS will be the only ones that have access to that route.

--- routes/videos.js
+++ routes/videos.js
@@ -1,24 +1,55 @@
 const router = require("express").Router();
 const Videos = require("../models/video_model");
+const restricted = require("../middleware/restricted_middleware");
 
-router.get("/video", async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const videos = await Videos.find();
-    res.json({ videos });
+    res.status(200).json({ videos });
   } catch (err) {
-    res.send({ message: "Try again later.", err });
+    res.status(500).json({ message: "Try again later.", err });
   }
 });
 
-router.get("/video/:id", async (req, res) => {
+router.get("/:id", async (req, res) => {
   const { id } = req.params;
 
   try {
     const video = await Videos.findById(id);
-    res.json(video);
+    res.status(200).json(video);
   } catch (err) {
-    res.send({ message: "Try again later.", err });
+    res.status(500).json({ message: "Try again later.", err });
   }
 });
+
+router.post("/", restricted, async (req, res) => {
+  const data = req.body;
+  const { video_title, location, song_id, user_id } = req.body;
+
+  try {
+    if (!video_title) {
+      res.status(400).json({ message: "PLease provide a Video Title!" });
+    } else {
+      if (!location) {
+        res.status(400).json({ message: "Please provide a location!" });
+      } else {
+        if (!song_id) {
+          res.status(400).json({ message: "Please provide a song!" });
+        } else {
+          if (!user_id) {
+            res.status(400).json({ message: "Please provide a user!" });
+          } else {
+            const video = await Videos.add(data);
+            res.status(200).json(video);
+          }
+        }
+      }
+    }
+  } catch (err) {
+    res.status(500).json({ message: "Try again later!", err });
+  }
+});
+
+
 
 module.exports = router;
