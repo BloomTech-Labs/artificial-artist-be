51cc8b0ff27c8b5f4d9b1045fe6a523b0eb601ce
Jimmy Hooker <jimmyjjwalker@gmail.com>
2020-05-12 21:11:23 -0700

    Adjusted the videos routes to test adding to both tables

--- routes/videos.js
+++ routes/videos.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const Videos = require("../models/video_model");
+const Songs = require("../models/song_model");
 const restricted = require("../middleware/restricted_middleware");
 
 router.get("/", async (req, res) => {
@@ -22,30 +23,71 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+// if you post to video
+// You want to check for main variables in two tables
+// Song table first because videos has a foreign key referencing the song.id
+// songs: deezer_id, title, artist_name
+// videos: video_title, location
+// We'll want to adjust the model to allow for both
+
+
 router.post("/", restricted, async (req, res) => {
-  const data = req.body;
-  const { video_title, location, song_id, user_id } = req.body;
+  // this should be unnecessary because we're rebuilding objects
+  // const data = req.body;
+  const { title_short, preview, artist, deezer_id, location, video_title, user_id } = req.body;
+
+  const songObject = {
+    deezer_id: deezer_id,
+    title: title_short,
+    artist_name: artist
+  }
+
+  console.log(songObject);
+
+  const videoObject = {
+    video_title: video_title,
+    location: location,
+    song_id: "",
+    user_id: user_id
+  };
+
+  console.log(videoObject);
 
   try {
-    if (!video_title) {
-      res.status(400).json({ message: "PLease provide a Video Title!" });
-    } else {
-      if (!location) {
-        res.status(400).json({ message: "Please provide a location!" });
-      } else {
-        if (!song_id) {
-          res.status(400).json({ message: "Please provide a song!" });
-        } else {
-          if (!user_id) {
-            res.status(400).json({ message: "Please provide a user!" });
-          } else {
-            const video = await Videos.add(data);
-            res.status(200).json(video);
-          }
-        }
-      }
-    }
-  } catch (err) {
+        const song = await Songs.add(songObject);
+
+        // console.log(song);
+
+        const videoObjectComplete = { ...videoObject, song_id: song };
+
+        // console.log(videoObjectComplete);
+        // we want song to return its id
+        // then we'll destructure or spread some object to add that in
+        // then we'll 'add' that object to videos.add
+        const video = await Videos.add(videoObjectComplete);
+
+        // console.log(video);
+
+        res.status(200).json(video);
+        // if (!video_title) {
+        //   res.status(400).json({ message: "PLease provide a Video Title!" });
+        // } else {
+        //   if (!location) {
+        //     res.status(400).json({ message: "Please provide a location!" });
+        //   } else {
+        //     if (!song_id) {
+        //       res.status(400).json({ message: "Please provide a song!" });
+        //     } else {
+        //       if (!user_id) {
+        //         res.status(400).json({ message: "Please provide a user!" });
+        //       } else {
+        //         const video = await Videos.add(data);
+        //         res.status(200).json(video);
+        //       }
+        //     }
+        //   }
+        // }
+      } catch (err) {
     res.status(500).json({ message: "Try again later!", err });
   }
 });
