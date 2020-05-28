f86d6bb357478aa9ae059f3ceed8a548078b291d
mickeybear <tuckerwray84@gmail.com>
2020-05-27 23:48:45 -0400

    kills updatevideo router-- going with the long poll over the putback from ds

--- api/api_router.js
+++ api/api_router.js
@@ -2,15 +2,13 @@ const router = require('express').Router();
 const restricted = require('../middleware/restricted_middleware');
 const authRouter = require('../auth/auth_router');
 const userRouter = require('../users/users_router');
-const video_update = require('../videos/video_update');
-const videos = require('../videos/videos_router');
-const songs = require('../songs/songs_router');
+const videosRouter = require('../videos/videos_router.js');
+const songsRouter = require('../songs/songs_router');
 
 router.use('/auth', authRouter);
 router.use('/users', restricted, userRouter);
-router.use('/update-video', video_update);
-router.use('/videos', videos);
-router.use('/songs', restricted, songs);
+router.use('/videos', videosRouter);
+router.use('/songs', restricted, songsRouter);
 
 router.get('/', (req, res) => {
     res.json('Artificial Artist API');
