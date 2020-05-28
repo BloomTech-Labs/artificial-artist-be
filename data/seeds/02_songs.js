52bef2bcaccd462c09963a9906b01567960c82fb
mickeybear <tuckerwray84@gmail.com>
2020-05-27 23:51:01 -0400

    updates songs seed shape

--- data/seeds/02_songs.js
+++ data/seeds/02_songs.js
@@ -4,39 +4,50 @@ exports.seed = function (knex, Promise) {
     .then(function () {
       return knex("songs").insert([
         {
-          deezer_id: "12",
+          deezer_id: 1,
           title: "Push Every Button",
           artist_name: "Mayhem",
         },
         {
-          deezer_id: "13",
+          deezer_id: 2,
           title: "Endeavours",
           artist_name: "Mitis",
         },
         {
-          deezer_id: "14",
+          deezer_id: 3,
           title: "See Me In A Crown",
           artist_name: "Billie Eilish",
         },
         {
-          deezer_id: "15",
+          deezer_id: 4,
           title: "ilomilo",
           artist_name: "Billie Eilish",
         },
         {
-          deezer_id: "16",
+          deezer_id: 5,
           title: "Future Bass",
           artist_name: "Runsblov",
         },
         {
-          deezer_id: "17",
+          deezer_id: 6,
           title: "Action Dubstep",
           artist_name: "Lazaoaza",
         },
         {
-          deezer_id: "18",
+          deezer_id: 7,
           title: "Funky Reggae Dubstep",
           artist_name: "Progressence",
+        },
+        {
+          deezer_id: 8,
+          title: "Drunk in Love",
+          artist_name: "Beyoncé",
+        },
+
+        {
+          deezer_id: 9,
+          title: "Don't Stop Believin'",
+          artist_name: "Journey",
         },
       ]);
     });
