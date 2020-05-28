d144a1f0a016901cbca9cdb8c274735f430f2f85
mickeybear <tuckerwray84@gmail.com>
2020-05-27 23:52:56 -0400

    updates videos seed shape to harness the mp4s we are actually delivering to the enduser. These are pulling from a test web s3 bucket not the DS bucket.

--- data/seeds/03_videos.js
+++ data/seeds/03_videos.js
@@ -1,3 +1,4 @@
+
 exports.seed = function (knex, Promise) {
   return knex("videos")
     .del()
@@ -5,44 +6,64 @@ exports.seed = function (knex, Promise) {
       return knex("videos").insert([
         {
           video_title: "Testing",
-          location: "https://www.youtube.com/watch?v=82Q6DRqf9H4",
+          location:
+            "https://elasticbeanstalk-us-east-1-427400502172.s3.amazonaws.com/Videosforbackend/mayhem--push_every_button-testing--82Q6DRqf9H4.mp4",
           song_id: 1,
           user_id: 1,
         },
         {
           video_title: "Trying",
-          location: "https://www.youtube.com/watch?v=VVrf89SXvTM",
+          location:
+            "https://elasticbeanstalk-us-east-1-427400502172.s3.amazonaws.com/Videosforbackend/mitis--endeavour-trying--VVrf89SXvTM.mp4",
           song_id: 2,
           user_id: 1,
         },
         {
           video_title: "King Me",
-          location: "https://www.youtube.com/watch?v=coLerbRvgsQ",
+          location:
+            "https://elasticbeanstalk-us-east-1-427400502172.s3.amazonaws.com/Videosforbackend/billieeilish--you_should_see_me_in_a_crown-king_me_-coLerbRvgsQ.mp4",
           song_id: 3,
           user_id: 2,
         },
         {
           video_title: "Weird",
-          location: "https://www.youtube.com/watch?v=-e7wiyNO2us",
+          location:
+            "https://elasticbeanstalk-us-east-1-427400502172.s3.amazonaws.com/Videosforbackend/billieeilish--ilomilo_weird_-e7wiyNO2us.mp4",
           song_id: 4,
           user_id: 2,
         },
         {
           video_title: "Boom",
-          location: "https://www.youtube.com/watch?v=h-f9Zgdo6I8&t",
+          location:
+            "https://elasticbeanstalk-us-east-1-427400502172.s3.amazonaws.com/Videosforbackend/runsblov--future_bass_-boom_h-f9Zgdo6I8%26t.mp4",
           song_id: 5,
           user_id: 3,
         },
         {
           video_title: "Dubs",
-          location: "https://www.youtube.com/watch?v=VqCm327iSwQ",
+          location:
+            "https://elasticbeanstalk-us-east-1-427400502172.s3.amazonaws.com/Videosforbackend/lazaoaza--action_dubstep_-dubs_VqCm327iSwQ.mp4",
           song_id: 6,
           user_id: 3,
         },
         {
           video_title: "Bud",
-          location: "https://www.youtube.com/watch?v=R2oxSIN6rdY",
+          location:
+            "https://elasticbeanstalk-us-east-1-427400502172.s3.amazonaws.com/Videosforbackend/progressencefunky_reggae_dubstep_-bud_R2oxSIN6rdY.mp4",
           song_id: 7,
+          user_id: 1,
+        },
+        {
+          video_title: "TarantulaCrisp",
+          location:
+            "https://elasticbeanstalk-us-east-1-427400502172.s3.amazonaws.com/Videosforbackend/TarantulaCrisp.mp4",
+          song_id: 8,
+          user_id: 1,
+        },
+        {
+          video_title: "the way of the web",
+          location: "https://artificial-artist.s3.amazonaws.com/10.mp4",
+          song_id: 9,
           user_id: 1,
         },
       ]);
