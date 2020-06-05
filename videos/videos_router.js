f809f26432ce9a77bc3c9b6330ed5aa7bdeb90a3
Jimmy Hooker <jimmyjjwalker@gmail.com>
2020-06-04 22:42:03 -0700

    Added s3checker function that changes video_created to true when it succeeds in getting the filename back from s3 after a post

--- videos/videos_router.js
+++ videos/videos_router.js
@@ -45,30 +45,54 @@ AWS.config.update({
 const s3 = new AWS.S3();
 
 // Check if S3 file exists
-router.get("/single/check-for-file", async (req, res) => {
-    const { fileName } = req.body;
-    console.log(req.body);
-    const params = {
-      Bucket: process.env.AWS_BUCKET,
-      Key: fileName,
-    };
-    console.log(params);
-    try {
-      s3.headObject(params, function (err, metadata) {
-        if (err && err.code === "NotFound") {
-          // Handle no object on cloud here
-          console.log(err);
+// router.get("/single/check-for-file", async (req, res) => {
+//     const { fileName } = req.body;
+//     console.log(req.body);
+//     const params = {
+//       Bucket: process.env.AWS_BUCKET,
+//       Key: fileName,
+//     };
+//     console.log(params);
+//     try {
+//       s3.headObject(params, function (err, metadata) {
+//         if (err && err.code === "NotFound") {
+//           // Handle no object on cloud here
+//           console.log(err);
+//         } else {
+//           s3.getSignedUrl("getObject", params, (err, data) => {
+//             if (err) res.status(500).json({ message: err });
+//             else     res.status(200).json({ message: data });
+//           });
+//         }
+//       });
+//     } catch (err) {
+//       res.status(500).json({ message: "You've found me", err });
+//     }
+// });
+
+const s3checker = (fileName, videoId) => {
+  const params = {
+    Bucket: process.env.AWS_BUCKET,
+    Key: fileName,
+  };
+  s3.headObject(params, function (err, metadata) {
+    if (err && err.code === "NotFound") {
+      // Handle no object on cloud here
+      console.log(err);
+    } else {
+      s3.getSignedUrl("getObject", params, (err, data) => {
+        if (err) {
+          setTimeout(() => {
+            res.status(500).json({ message: err })
+          }, 10000);
         } else {
-          s3.getSignedUrl("getObject", params, (err, data) => {
-            if (err) res.status(500).json({ message: err });
-            else     res.status(200).json({ message: data });
-          });
+          Videos.update({ video_created: true }, videoId);
+          res.status(200).json({ message: data })
         }
       });
-    } catch (err) {
-      res.status(500).json({ message: "You've found me", err });
     }
-});
+  });
+}
 
 // if you post to video
 // You want to check for main variables in two tables
@@ -107,8 +131,8 @@ router.post("/", restricted, async (req, res) => {
 
   const videoObject = {
     video_title: video_title,
-    // location will be known and use a UUID plus this https://artificial-artist.s3.amazonaws.com/
     location: "",
+    thumbnail: "",
     song_id: "",
     user_id: user_id,
   };
@@ -126,9 +150,6 @@ router.post("/", restricted, async (req, res) => {
           if (!deezer_id) {
             res.status(400).json({ message: "Missing deezer_id!" });
           } else {
-            if (!location) {
-              res.status(400).json({ message: "Missing location!" });
-            } else {
               if (!video_title) {
                 res.status(400).json({ message: "Missing video_title!" });
               } else {
@@ -151,8 +172,7 @@ router.post("/", restricted, async (req, res) => {
                   // then we'll 'add' that object to videos.add
                   const video = await Videos.add(videoObjectComplete);
                   
-                  
-                  axios
+                  await axios
                     .post(
                       "http://artificial-artist.eba-cyfpphb2.us-east-1.elasticbeanstalk.com/entry",
                       null,
@@ -172,17 +192,23 @@ router.post("/", restricted, async (req, res) => {
 
                   console.log(objectIds);
 
-                  res.status(200).json(objectIds);
+                  s3checker(videoId, video);
+
+                  res
+                    .status(200)
+                    .json({
+                      message: "Successfully created video!",
+                      objectIds,
+                    });
                 }
               }
             }
           }
         }
       }
-    }
   } catch (err) {
     console.log(err);
-    res.status(500).json({ message: "Try again later 3", err });
+    res.status(500).json({ message: "Could not post video", err });
   }
 });
 
