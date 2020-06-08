926af176768948e86b2545a0c855723083b57ff0
Jimmy Hooker <jimmyjjwalker@gmail.com>
2020-06-07 21:01:18 -0700

    Adjusted model for videos so we can see result of successful post

--- videos/videos_model.js
+++ videos/videos_model.js
@@ -30,6 +30,7 @@ function find() {
       "videos.id",
       "videos.video_title",
       "videos.location",
+      "videos.video_created",
       "videos.song_id",
       "songs.title",
       "songs.artist_name"
