67cc2a63fa644a9ac1cf93404a7d7d1260969106
Jimmy Hooker <jimmyjjwalker@gmail.com>
2020-06-07 20:42:06 -0700

    Adjusted back to dev

--- data/db_config.js
+++ data/db_config.js
@@ -2,7 +2,7 @@ const knex = require('knex');
 
 const knexfile = require('../knexfile');
 
-const env = process.env.DB_ENV || 'production';
+const env = process.env.NODE_ENV || 'development';
 
 const configOptions = knexfile[env];
 
