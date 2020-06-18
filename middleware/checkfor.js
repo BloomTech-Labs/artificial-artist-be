91ba13fdcda3060f62714eb77abd401fbd27751b
tucker <tuckerwray84@gmail.com>
2020-06-17 22:58:01 -0400

    cleansup console.logs

--- middleware/checkfor.js
+++ middleware/checkfor.js
@@ -2,14 +2,12 @@ module.exports = (props) => (req, res, next) => {
   let presentParams = [];
   let missingParams = [];
   let requestValues = Object.keys(req.body);
-  console.log("line5", { requestValues });
+
   props.map((bodyParam) => {
     if (requestValues.includes(bodyParam)) {
       presentParams = [...presentParams, bodyParam];
-      console.log("line9", { presentParams });
     } else {
       missingParams = [...missingParams, bodyParam];
-      console.log("line12", { missingParams });
     }
   });
 
