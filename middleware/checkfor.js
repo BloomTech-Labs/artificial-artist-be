a5a43a0cdca0e3cf61c96a75d885f83c930a51d8
tucker <tuckerwray84@gmail.com>
2020-06-17 22:33:35 -0400

    patches checkfor middleware--gets rid of the adds header after request error

--- middleware/checkfor.js
+++ middleware/checkfor.js
@@ -1,10 +1,25 @@
-module.exports = (props) => (req, res, next) =>
-  props.map((prop) =>
-    req.body[prop]
-      ? next()
-      : res
-          .status(400)
-          .json({
-            errorMessage: `The ${prop} is required to create a video. Please include a ${prop} and try again.`,
-          })
-  );
+module.exports = (props) => (req, res, next) => {
+  let presentParams = [];
+  let missingParams = [];
+  let requestValues = Object.keys(req.body);
+  console.log("line5", { requestValues });
+  props.map((bodyParam) => {
+    if (requestValues.includes(bodyParam)) {
+      presentParams = [...presentParams, bodyParam];
+      console.log("line9", { presentParams });
+    } else {
+      missingParams = [...missingParams, bodyParam];
+      console.log("line12", { missingParams });
+    }
+  });
+
+  if (missingParams.length > 0) {
+    const errorMsg = `The ${missingParams} is required to create a video. Please include ${missingParams} and try again.`;
+    const error = new Error(errorMsg);
+    error.httpStatusCode = 400;
+    return next(error);
+  }
+  if (presentParams.length == props.length) {
+    return next();
+  }
+};
